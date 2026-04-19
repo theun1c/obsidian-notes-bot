3cc2ffe4795732d9cb9a798dc94618c7f7ebcd84
theun1c <belovdima1122@yandex.ru>
2026-04-19 18:16:06 +0300

    feat: create echo bot

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Start")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env not load")
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(os.Getenv("TOKEN"), opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
